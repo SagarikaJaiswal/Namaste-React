35480f03d4c8c2d6d1651dd4ecb3b67a9739ca8f
Sagarika Jaiswal <sagarikajaiswal97@gmail.com>
2023-04-23 12:56:45 +0530

    episode-03 Laying the foundation

--- App.js
+++ App.js
@@ -14,12 +14,36 @@ import ReactDOM from "react-dom/client";
 */
 
 //const heading = React.createElement("h1", { id: "heading" }, "Hello World from React");
-const parent = React.createElement(
-    "div", { id: "parent" }, [React.createElement("div", { id: "child1" }, [React.createElement("h1", {}, "H1 tag"), React.createElement("h2", {}, "H2 tag")]),
-        React.createElement("div", { id: "child1" }, [React.createElement("h1", {}, "H1 tag"), React.createElement("h2", {}, "H2 tag")])
-    ]
-)
-console.log(parent);
+// const parent = React.createElement(
+//     "div", { id: "parent" }, [React.createElement("div", { id: "child1" }, [React.createElement("h1", {}, "H1 tag"), React.createElement("h2", {}, "H2 tag")]),
+//         React.createElement("div", { id: "child1" }, [React.createElement("h1", {}, "H1 tag"), React.createElement("h2", {}, "H2 tag")])
+//     ]
+// )
+// console.log(parent);
+
+//React Element
+const heading = (
+  <h1 className="head" tabIndex="5">
+    This is a React Element using JSX
+  </h1>
+);
+const Title = () => <h1>Component embedded inside another component</h1>;
+//React Functional Component
+const HeadingComponent = () => (
+  <React.Fragment>
+    <div id="container">
+      {/* Component Composition */}
+      <Title />
+      <h1 className="head">Namaste React using Functional Component</h1>
+    </div>
+  </React.Fragment>
+);
+// -----------------------------------------------------------------------
 //the heading here is the react element not a html tag
+//const jsxHeading = <h1 className="heading"> Namaste React in JSX </h1>;
+// -----------------------------------------------------------------------
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+// root.render(heading);
+
+// Rendering Functional Component
+root.render(<HeadingComponent />);
